e826bec85fdf1be1257a745fae423187ff69cdc1
Hassan Jamil <hassanjamil91@gmail.com>
2022-02-17 21:32:56 +0400

    Removed unnecessary hilt instrumented test

--- app/src/androidTest/java/com/nytimes/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nytimes/app/ExampleInstrumentedTest.kt
@@ -1,22 +1,10 @@
 package com.nytimes.app
 
-import androidx.fragment.app.viewModels
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.nytimes.app.data.remote.ApiHelper
-import com.nytimes.app.data.remote.ApiService
-import com.nytimes.app.ui.home.HomeViewModel
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import retrofit2.Retrofit
-import javax.inject.Inject
 
 
 /**
@@ -27,30 +15,11 @@ import javax.inject.Inject
  * @author Hassan Jamil
  */
 @RunWith(AndroidJUnit4::class)
-@HiltAndroidTest
 class ExampleInstrumentedTest {
-    @Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Inject
-    lateinit var apiHelper: ApiHelper
-
-    @Before
-    fun init() {
-        hiltRule.inject()
-    }
-
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.nytimes.app", appContext.packageName)
-    }
-
-    @ExperimentalCoroutinesApi
-    @Test
-    fun testApi() = runBlocking {
-        val response = apiHelper.getMostPopularArticles()
-        assertEquals("OK", response.status)
     }
 }
